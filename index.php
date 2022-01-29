9785def36717fd75a0aa4bdb6f9b0c6c1ce82731
Armandok77 <armandokarim02@gmail.com>
2022-01-29 14:10:38 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -9,5 +9,9 @@
 <body>
     <h1>Hello World!</h1>
     <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Doloribus, explicabo quod omnis aliquam mollitia placeat! Dolor voluptas recusandae autem! Consequatur illum consectetur magnam architecto ad rem ipsam accusamus at id?</p>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+    </ul>
 </body>
 </html>
